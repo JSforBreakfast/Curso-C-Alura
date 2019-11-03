102fa946c1b59985180d8b85131b7ffe2df235e2
Vitor Emanuel <veslima3@gmail.com>
2019-11-03 20:14:55 -0300

    Update advinhacao.c

--- Curso_C_Alura/advinhacao.c
+++ Curso_C_Alura/advinhacao.c
@@ -3,7 +3,9 @@
 int main(){
     //imprime o cabecalho do jogo
     printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
-    printf("x Bem vindo ao jogo de adivinhacao. x\n");
+    printf("x Bem vindo ao jogo de adivinhacao  x\n");
     printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
 }
+
+int numSec = 42;
 
