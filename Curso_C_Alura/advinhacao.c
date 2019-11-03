a02bcc19c0e50d366361608f68907d732a84b8ed
Vitor Emanuel <veslima3@gmail.com>
2019-11-03 19:33:09 -0300

    Add files via upload

---
+++ Curso_C_Alura/advinhacao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    //imprime o cabecalho do jogo
+    printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+    printf("x Bem vindo ao jogo de adivinhacao. x\n");
+    printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+}
+
