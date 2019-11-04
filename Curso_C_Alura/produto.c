671029831e1fe7f1882c23efe96cb63a83ac5d01
Vitor Emanuel <veslima3@gmail.com>
2019-11-03 21:30:11 -0300

    Update produto.c

--- Curso_C_Alura/produto.c
+++ Curso_C_Alura/produto.c
@@ -14,3 +14,4 @@ int main() {
   int prod = x * y;
   printf("O produto de %d por %d e igual a %d\n", x, y, prod);
 }
+//a ordem de declaração das variáveis importa. É necessário declarar as variáveis que serão multiplicadas antes do printf do produto delas e declarar a variavel que indica a multiplicação antes do printf do produto final.
