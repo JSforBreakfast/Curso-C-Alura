ff597991e81f94f11dc64e4cb460f2a5693e97eb
Vitor Emanuel <veslima3@gmail.com>
2019-11-24 16:51:52 -0300

    Create adivinhacao2.c

---
+++ Curso_C_Alura/adivinhacao2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#define TENTATIVAS 5
+
+int main(){
+    //imprime o cabecalho do jogo
+    printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+    printf("x Bem vindo ao jogo de adivinhacao  x\n");
+    printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n\n");
+
+    int numSec = 42;
+    //int TENTATIVAS = 5;
+    int chute, tentativas = 1;
+    int ganhou = 0;
+    
+    while(ganhou == 0) {
+        
+        printf("Tentativa %d.\n", tentativas);
+        printf("Qual é o seu chute?\n");
+        
+        scanf("%d", &chute);
+        
+        if(chute < 0){
+        printf("Você não pode chutar números negativos.\n");
+        continue;
+        }
+        
+        int acertou = (chute == numSec);
+        int maior = chute > numSec;
+        //int menor = chute < numSec;
+        
+        if(acertou){
+        
+        printf("Parabéns! Você acertou!\n\n");
+        
+        ganhou == 1;
+        break;
+        } 
+        
+        else if(maior){
+                printf("Seu chute foi maior que o número secreto.\n\n");
+        }
+        
+        else {
+                printf("Seu chute foi menor que o número secreto.\n\n");
+        }
+        
+        tentativas += 1;
+    }
+    
+    printf("\n\nFim de jogo!\n");
+    printf("Você acertou em %d tentativas.", tentativas);
+}
